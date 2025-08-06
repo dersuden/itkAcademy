2784bb9dabc6dd1d191d85c5c5f49919cb45a0f6
Arthur Valiev <xylophone909@gmail.com>
2025-08-06 21:26:32 +0300

    Проблема с тестами решена, mvn package отрабатывает корректно без необходимости пропускать тесты. При этом начало выбивать ошибку у Liquibase, пришлось фиксить. Регресс тест пройден как в IDE, так и через контейнер в докере. Всё отрабатывает безошибочно.

--- src/test/java/org/example/itkacademy/ItkAcademyApplicationTests.java
+++ src/test/java/org/example/itkacademy/ItkAcademyApplicationTests.java
@@ -2,8 +2,12 @@ package org.example.itkacademy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.properties")
+@ActiveProfiles("test")
 class ItkAcademyApplicationTests {
 
     @Test
